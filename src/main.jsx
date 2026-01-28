c9da6d49e9676e12ab971519db49acd5c6540407
Reality Patchnotes <92701089+Vibeaxis@users.noreply.github.com>
2026-01-28 11:08:34 -0500

    Change root element ID from 'root' to 'app'

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Match this to the ID in your index.html
+ReactDOM.createRoot(document.getElementById('app')).render(
   <App />
 );
